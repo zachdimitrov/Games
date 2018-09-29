82471602ee4b4235bfae5a5bde1eddb9ae3d1ef0
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2018-09-29 17:13:21 +0300

    finish pong game

--- Udemy Course/04. Pong/game.js
+++ Udemy Course/04. Pong/game.js
@@ -15,6 +15,13 @@ let paddleHeight = 100;
 let speedX = 10;
 let speedY = 5;
 
+let player1score = 0;
+let player2score = 0;
+let winScore = 3;
+
+let enemySpeed = 12;
+let showWinScreen = false;
+
 window.onload = function() {
     canvas = document.getElementById("game");
     context = canvas.getContext("2d");
@@ -26,10 +33,20 @@ window.onload = function() {
     }, 1000 / framesPerSecond);
 
     document.addEventListener("keydown", keyPressed);
+
+    canvas.addEventListener("mousedown", handleMouseClick);
     canvas.addEventListener("mousemove", function(evt) {
         let mousePos = calculateMousePos(evt);
         paddle1Y = mousePos.y - paddleHeight / 2;
     });
+}
+
+function handleMouseClick(evt) {
+    if (showWinScreen) {
+        player1score = 0;
+        player2score = 0;
+        showWinScreen = false;
+    }
 }
 
 function keyPressed(evt) {
@@ -53,18 +70,47 @@ function calculateMousePos(evt) {
     };
 }
 
+function computerMovement() {
+    if (paddle2Y + paddleHeight / 2 < ballY - paddleHeight / 3) {
+        paddle2Y += enemySpeed;
+    } else if (paddle2Y + paddleHeight / 2 > ballY + paddleHeight / 3) {
+        paddle2Y -= enemySpeed;
+    }
+}
+
 function moveEverything() {
+    if (showWinScreen) {
+        return;
+    }
+
+    computerMovement();
+
     ballX += speedX;
     ballY += speedY;
 
-    if (ballX <= paddle1X + paddleThickness + ballSize / 2 &&
-        (ballY > paddle1Y && ballY < paddle1Y + paddleHeight)) {
-        speedX *= -1;
+    if (ballX < paddle1X + paddleThickness + ballSize) {
+        if (ballY > paddle1Y && ballY < paddle1Y + paddleHeight) {
+            speedX *= -1;
+            let deltaY = ballY - (paddle1Y + paddleHeight / 2);
+            speedY = deltaY * 0.35;
+        } else if (ballX <= ballSize / 2) {
+            player2score++;
+            ballReset();
+        }
     }
 
-    if (ballX <= 0) { ballReset(); }
-    if (ballX > canvas.width - ballSize) { speedX *= -1 }
-    if (ballY > canvas.height - ballSize || ballY == 0) { speedY *= -1 }
+    if (ballX >= paddle2X - ballSize / 2) {
+        if (ballY > paddle2Y && ballY < paddle2Y + paddleHeight) {
+            speedX *= -1;
+            let deltaY = ballY - (paddle2Y + paddleHeight / 2);
+            speedY = deltaY * 0.35;
+        } else if (ballX >= canvas.width - ballSize / 2) {
+            player1score++;
+            ballReset();
+        }
+    }
+
+    if (ballY >= canvas.height - ballSize || ballY <= 0) { speedY *= -1 }
 }
 
 function colorRect(leftX, topY, width, height, drawColor) {
@@ -79,15 +125,51 @@ function colorCircle(centerX, centerY, diameter, drawColor) {
     context.fill();
 }
 
+function drawNet() {
+    for (let i = 0; i < canvas.height; i += 30) {
+        colorRect(canvas.width / 2 - 1, i, 2, 12, "white");
+    }
+}
+
 function drawEverything() {
     colorRect(0, 0, canvas.width, canvas.height, "black");
-    colorRect(paddle1X, paddle1Y, paddleThickness, paddleHeight, "white");
-    colorRect(canvas.width - paddle1X - paddleThickness, paddle2Y, paddleThickness, paddleHeight, "white");
-    colorCircle(ballX, ballY, ballSize, "red");
+    if (showWinScreen) {
+        context.fillStyle = "white";
+        let player = player1score > player2score ? "ЛЕВИЯ" : "ДЕСНИЯ";
+        context.font = "30px Arial";
+        context.fillText(player + " ИГРАЧ ПОБЕДИ!", 230, 200);
+        context.font = "20px Arial";
+        context.fillText("ЩРАКНЕТЕ ЗА НОВА ИГРА!", 270, 250);
+        return;
+    }
+
+    drawNet();
+    colorRect(paddle1X, paddle1Y, paddleThickness, paddleHeight, "cyan");
+    colorRect(canvas.width - paddle1X - paddleThickness, paddle2Y, paddleThickness, paddleHeight, "orange");
+    colorCircle(ballX, ballY, ballSize, "yellowgreen");
+
+    context.fillStyle = "white";
+    context.font = "20px Arial";
+    context.fillText(player1score, 200, 100);
+    context.fillText(player2score, canvas.width - 200, 100);
 }
 
 function ballReset() {
-    speedX *= -1;
+    if (player1score >= winScore || player2score >= winScore) {
+        showWinScreen = true;
+    }
+
+    let tempSpeedX = speedX;
+    let tempSpeedY = speedY;
+
+    speedX = 0;
+    speedY = 0;
+
     ballX = canvas.width / 2;
     ballY = canvas.height / 2;
+
+    setTimeout(() => {
+        speedX = tempSpeedX * -1;
+        speedY = tempSpeedY;
+    }, 1500);
 }
